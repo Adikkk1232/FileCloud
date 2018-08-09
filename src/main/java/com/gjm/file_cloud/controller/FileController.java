0c4bf5d37c06cb04b130161d7b5cbb0c6c6a86af
gjm <gim18.malyska.grzegorz@gmail.com>
2018-08-09 20:58:46 +0200

    Added Spring MVC'S FileController tests.

--- src/main/java/com/gjm/file_cloud/controller/FileController.java
+++ src/main/java/com/gjm/file_cloud/controller/FileController.java
@@ -18,8 +18,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-// TODO: Write test for this MVC controller.
-
 @Controller
 public class FileController {
     private FileService fileService;
@@ -29,7 +27,7 @@ public class FileController {
         this.fileService = fileService;
     }
 
-    @RequestMapping(value = {"/", "/home", "/index"}, method = RequestMethod.GET)
+    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
     public String home() {
         return "index";
     }
